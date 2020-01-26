2fa5ff841ee40d96b401e3ea7bcc42ac8efd26e8
Mateusz Szubski <szubskimateusz@gmail.com>
2020-01-26 18:56:08 +0100

    Dodanie biblioteki Angular Google Maps + pierwsze zastosowanie na stronie Kontakt.

--- src/main/webapp/app/entities/kontakt/kontakt.component.ts
+++ src/main/webapp/app/entities/kontakt/kontakt.component.ts
@@ -4,7 +4,18 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './kontakt.component.html'
 })
 export class KontaktComponent implements OnInit {
+  longitude = 7.809007;
+  latitute = 51.678418;
+  locationChosen = false;
+
   constructor() {}
 
   ngOnInit() {}
+
+  onChoseLocation(event: any) {
+    console.log(event);
+    this.latitute = event.coords.lat;
+    this.longitude = event.coords.lng;
+    this.locationChosen = true;
+  }
 }
--- src/main/webapp/app/entities/kontakt/kontakt.module.ts
+++ src/main/webapp/app/entities/kontakt/kontakt.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MagazynGrzewczySharedModule } from 'app/shared/shared.module';
+import { AgmCoreModule } from '@agm/core';
 
 import { KontaktComponent } from './kontakt.component';
 
 import { kontaktRoute } from './kontakt.route';
 
 @NgModule({
-  imports: [MagazynGrzewczySharedModule, RouterModule.forChild([kontaktRoute])],
+  imports: [
+    MagazynGrzewczySharedModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyDn6LWAKvHbrEuiBPs6bYKPfFXCSBFiGhs'
+    }),
+    RouterModule.forChild([kontaktRoute])
+  ],
   declarations: [KontaktComponent]
 })
 export class KontaktModule {}
