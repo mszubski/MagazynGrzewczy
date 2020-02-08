80914e48c3e31e6e7f4cac46a2c891509e4d7dc3
Mateusz Szubski <szubskimateusz@gmail.com>
2020-02-08 15:41:32 +0100

    Usunięcie wstążki oznaczającej środowisko, dodanie ProduktKategoriaEnum

---
+++ src/main/java/com/vistula/magazyn/domain/enumeration/ProduktKategoriaEnum.java
@@ -0,0 +1,8 @@
+package com.vistula.magazyn.domain.enumeration;
+
+/**
+ * The ProduktKategoriaEnum enumeration.
+ */
+public enum ProduktKategoriaEnum {
+    GRZEJNIKI, PIECE, KOMINKI, AKCESORIA
+}
